b2875069754592202eef79b7173ba86dbe2ebabe
Winston Smith <touchedouble@gmx.de>
2024-04-30 09:38:49 +0200

    bugfix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=['pandas', 'numpy', 'scikit-learn', 'lifelines'],
     description='Collection of useful classes and functions',
     author='Jan P. Hummel',
-    author_email='jan.hummel@med.uni-muenchen.de'
+    author_email='jan.hummel@med.uni-muenchen.de',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
